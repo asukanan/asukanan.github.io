b334204e3b0e277c336dc251ebebacbc1ced52e9
asukanan <asukawoo@gmail.com>
2025-05-18 14:33:00 +0800

    Site updated: 2025-05-18 14:33:00

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2025/03/15/原摄之芙宁娜/","2025/03/17/Mac安装homebrew/","2025/04/24/snell简单搭建/","2025/05/05/yt-dlp简单配置/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
